dd5e04dcda67ee37f5feb6dca1702006e9ebd65b
Amar Sood <mail@tekacs.com>
2011-08-26 01:48:32 +0100

    Added extension transforms

--- download.rb
+++ download.rb
@@ -14,9 +14,15 @@ FORMAT = ARGV[0].to_sym
 cfa = CFAgent.new
 die "Please login using login.rb first." unless cfa.login
 
+def ext(format)
+  return :html if format == :print
+  return :txt if format == :text
+  return :format
+end
+
 for name in ARGV[1, ARGV.length]
   id = cfa.outline_ids_by_name(name)[0]
-  open("#{name}.#{FORMAT}", 'w') do |f|
+  open("#{name}.#{ext(FORMAT)}", 'w') do |f|
     f.write(cfa.get(id, FORMAT))
   end
 end
