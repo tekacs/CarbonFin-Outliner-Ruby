03b360b7d7931671b23553382eaa7e864842f67f
Amar Sood <mail@tekacs.com>
2011-08-26 11:19:03 +0100

    Added non-OPML upload warning.

--- upload.rb
+++ upload.rb
@@ -2,6 +2,7 @@
 require './carbonfin.rb'
 
 USAGE = "Usage: upload.rb <file0.opml> <file1.opml> <file2.opml> ..."
+OPML_WARNING = "Warning: You should only upload OPML."
 
 def die(msg)
   warn msg
@@ -15,6 +16,7 @@ die "Please login using login.rb first." unless cfa.login
 
 for filename in ARGV
   if File.exists? filename
+    warn OPML_WARNING if (File.extname filename) != '.opml'
     cfa.upload_file(filename)
   end
 end
